18a0f6689f74048146e4853508421cc025730344
DevByShrirang <shrirang.patil1812@gmail.com>
2025-11-06 19:37:00 +0530

    add

--- Abhishekh.python/Day_1/10_string_replace.py
+++
@@ -1,7 +0,0 @@
-#  .replace() is a string method used to replace a substring with another substring.
-
-text = "interview was hard"
-
-new_text = text.replace("hard", "superb")
-
-print("modified text:", new_text)
